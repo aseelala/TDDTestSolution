c3d435741aa1356142817de8eaca231156f035a5
Aseel Alaia <aseel.alaia@vdabcampus.be>
2021-01-05 10:31:49 +0100

    Add project files.

---
+++ TDDTestLibrary/Persoon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TDDTestLibrary
+{
+    public class Persoon
+    {
+        private List<String> Voornamen = new List<String>();
+
+        private static string pattern = @"^.*\S.*$";
+        private static readonly Regex regex = new Regex(pattern);
+
+
+        public Persoon(List<String> voornamen)
+        {
+            if (voornamen == null)
+                throw new ArgumentException();
+            if(voornamen.Count<=0)
+                throw new ArgumentException();
+            
+            foreach(var voornaam in voornamen)
+                {
+                if (!regex.IsMatch(voornaam))
+                    throw new ArgumentException();
+
+                }
+            if (voornamen.Count != voornamen.Distinct().Count())
+            {
+                    throw new ArgumentException();
+
+            }
+
+
+            Voornamen = voornamen;
+
+
+        }
+
+
+        public override string ToString() 
+        {
+            string result="";
+            foreach (var voornaam in Voornamen)
+            {
+                if (result != "")
+                    result += " ";
+                result += voornaam;
+            }
+            return result;
+        }
+    }
+}
---
+++ TDDTestTesting/PersoonTest.cs
@@ -0,0 +1,88 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using TDDTestLibrary;
+
+
+namespace TDDTestTesting
+{
+    [TestClass]
+    public class PersoonTest
+    {
+        private List<string> naam;
+        private Persoon persoon1;
+      
+        [TestInitialize]
+        public void Initialize()
+        {
+            naam = new List<string>();
+        }
+      
+
+        [TestMethod]
+        public void normalUitVoerenMetListVanVoornamen()
+        {
+            naam.Add(" Ana");
+            naam.Add("Liza");
+            naam.Add("Maria");
+
+            persoon1 = new Persoon(naam);
+
+            string persoonVooranaam="";
+            foreach(var voornaam in naam)
+            {
+                if (persoonVooranaam != "")
+                    persoonVooranaam += " ";
+
+                persoonVooranaam += voornaam;
+
+            }
+            Assert.AreEqual(persoonVooranaam, persoon1.ToString());
+        }
+
+
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+
+        public void voornamenListMoetMinsteEenVoornaamBevat()
+        {
+            persoon1 = new Persoon(naam);
+            
+        }
+
+
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+
+        public void voornamenListMetEenVoornaamBevatMistenEenTeken()
+        {            
+            naam.Add("Liza");
+            naam.Add("Maria");
+            naam.Add("");
+            persoon1 = new Persoon(naam);
+
+        }
+
+
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void ListVanVoornamenBevatNietHerhalendeVoornaman()
+        {
+            naam.Add("Maria");
+            naam.Add("Liza");
+            naam.Add("Maria");
+            persoon1 = new Persoon(naam);
+            
+        }
+
+
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void NullListVoornamenNotAccepted()
+        {
+            naam = null;
+            persoon1 = new Persoon(naam);
+        }
+
+    }
+}
